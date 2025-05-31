72c9a41fd79093681673557916dba7a6f72f08c4
Utsav Kumar <95420144+jhautsav14@users.noreply.github.com>
2025-05-31 13:59:45 +0530

    updated rentals

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "323191055"
+    serviceWorkerVersion: "3885499134"
   }
 });
